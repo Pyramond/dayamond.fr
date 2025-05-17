648d3e2a037667ccf50317066b359a67227fbbe7
Pyramond <pyramond69@gmail.com>
2025-05-17 22:56:54 +0200

    feat: separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Profile from "@/components/profile/profile"
+import Separator from "@/components/separator/separator";
 
 export default function Home() {
   return (
-      <>
+      <div className="flex flex-row">
           <Profile />
-      </>
+          <Separator />
+      </div>
   );
 }
