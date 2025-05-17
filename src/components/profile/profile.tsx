5f54eebd315cd7680242f33b36a60cfae58b9d00
Pyramond <pyramond69@gmail.com>
2025-05-17 18:44:09 +0200

    feat: social link

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,9 +1,12 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./profile.module.css"
 
 export default function profile() {
 
     const PROFILE_PICTURE = "/profile-picture.png";
+    const GITHUB_LINK = "https://github.com/Pyramond"
 
     return (
         <div className={styles.profile}>
@@ -21,6 +24,19 @@ export default function profile() {
                 <h2 className={styles.info}> Quentin T'JAMPENS </h2>
                 <h2 className={styles.info}> 19 ANS </h2>
                 <h2 className={styles.info}> BUT INFO, LYON 1 </h2>
+            </div>
+
+            <div className={styles.socials}>
+
+                <Image
+                    src={"/GitHub_dark.svg"}
+                    width={50}
+                    height={50}
+                    alt={"GutHub dark"}
+                    onClick={() => { window.open(GITHUB_LINK, '_blank') }}
+                    className={styles.social}
+                />
+
             </div>
 
         </div>
