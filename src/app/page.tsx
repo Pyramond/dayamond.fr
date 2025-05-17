ab97d3724f66be55a90af254148dc7be55f68d3c
Pyramond <pyramond69@gmail.com>
2025-05-17 18:25:28 +0200

    feat: Create Profile componnent with profile-picture and general informations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import Profile from "@/components/profile/profile"
 
 export default function Home() {
   return (
       <>
+          <Profile />
       </>
   );
 }
