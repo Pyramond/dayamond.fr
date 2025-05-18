cdab30f77e6c2d7392cc4a940c24aae2650a8b8c
Pyramond <pyramond69@gmail.com>
2025-05-18 15:46:34 +0200

    feat: Create stack component

---
+++ src/components/stack/stack.tsx
@@ -0,0 +1,11 @@
+import styles from "./stack.module.css";
+
+export default function Stack() {
+    return (
+        <div className={styles.stack}>
+            <h1 className={styles.title}>skills</h1>
+
+
+        </div>
+    );
+}
