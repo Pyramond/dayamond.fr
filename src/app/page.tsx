8db169a0b86610bd46ea3d18fe5d07a81d753c6b
Pyramond <pyramond69@gmail.com>
2025-05-18 15:46:06 +0200

    feat: add context + can now change component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
+'use client'
+
+import { useContext } from "react";
+import { PageContext } from "@/context/page-context";
 import Presentation from "@/components/presentation/presentation";
 import Profile from "@/components/profile/profile"
 import Separator from "@/components/separator/separator";
+import Stack from "@/components/stack/stack";
+
 
 export default function Home() {
+
+    const { value, setValue } = useContext(PageContext);
+
+    const components = [<Presentation />, <Stack />];
+
   return (
       <div className="flex flex-row">
           <Profile />
           <Separator />
-          <Presentation />
+          {components[value+1]}
       </div>
   );
 }
