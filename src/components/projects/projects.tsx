76ef1b5f81aec441e6e0a2d26942c0c342496622
Pyramond <pyramond69@gmail.com>
2025-05-19 00:03:30 +0200

    feat: display projects list

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,10 +1,15 @@
 import styles from "./projects.module.css"
+import ProjectCard from "@/components/projects/projectCard";
 
 export default function Projects() {
 
     return (
         <div className={styles.projects}>
             <h1 className={styles.title}>mes projets</h1>
+
+            <div className={styles.projectsList}>
+
+            </div>
         </div>
     )
 }
