46c541c51e481456a4872c10f0e83e7601ec91c3
Pyramond <pyramond69@gmail.com>
2025-05-20 22:01:44 +0200

    fix: remove fetch in getPresentation

--- src/components/presentation/presentation.tsx
+++ src/components/presentation/presentation.tsx
@@ -1,12 +1,17 @@
 import styles from "./presentation.module.css";
+import { promises as fs } from "fs";
+import path from "path";
+
 
 interface Presentation {
     presentation: string;
 }
 
 async function getPresentation(): Promise<Presentation> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/presentation.json`, { cache: "no-store" });
-    const data = await res.json();
+
+    const filePath = path.join(process.cwd(), "public", "presentation.json");
+    const file = await fs.readFile(filePath, "utf-8");
+    const data = JSON.parse(file);
     return data || {};
 }
 
