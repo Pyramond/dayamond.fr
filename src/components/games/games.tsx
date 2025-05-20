83ebc97f96dae8c5fb78644528406077868a7f89
Pyramond <pyramond69@gmail.com>
2025-05-20 23:56:27 +0200

    feat(games): display games

--- src/components/games/games.tsx
+++ src/components/games/games.tsx
@@ -1,11 +1,11 @@
-import styles from "./games.module.css";
+import path from "path";
+import { promises as fs } from "fs";
+import GamesList from "./GameList";
 
-export default function Games() {
-
-    return (
-        <div className={styles.games}>
-            <h1 className={styles.title}>Mes jeux favoris</h1>
-
-        </div>
-    );
+export default async function Games() {
+    const filePath = path.join(process.cwd(), "public", "games.json");
+    const file = await fs.readFile(filePath, "utf-8");
+    const data = JSON.parse(file);
+    const games = data["games"] || [];
+    return <GamesList games={games} />;
 }
