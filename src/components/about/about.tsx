ba5f8d028b62fc88858d2964f726149d6b225a3d
Pyramond <pyramond69@gmail.com>
2025-05-25 17:36:01 +0200

    feat: create site section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,17 @@
 import styles from "./about.module.css"
 import Contact from "./contact/contact"
+import Iconography from "@/components/about/iconography/iconography";
 
 export default function About() {
     return (
         <div className={styles.about}>
             <h1 className={styles.title}>A propos</h1>
             <Contact />
+
+            <div className={styles.site}>
+                <h2 className={styles.subtitle}>Site</h2>
+                <Iconography />
+            </div>
         </div>
     )
 }
