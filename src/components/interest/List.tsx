40926b75b0ae222d04ef45b68ed9b08426bc932c
Pyramond <pyramond69@gmail.com>
2025-06-27 17:25:15 +0200

    fix: hidden border when hover image

--- src/components/interest/List.tsx
+++ src/components/interest/List.tsx
@@ -20,21 +20,29 @@ export default function GamesList({ items, title, direction, type, SIZE }: Props
 
         <h2 className={styles.subtitle}>{title}</h2>
 
-        <Marquee pauseOnHover={true} speed={30} direction={direction ? direction : "right"}>
-            {items.map((item, index) => (
-                <MotionImage
-                    src={`/images/${type}/${item}.jpg`}
-                    width={SIZE}
-                    height={1}
-                    alt={item}
-                    key={index}
-                    className={styles.item}
-                    whileHover={{
-                        scale: 1.03,
-                        transition: { duration: 0.3 }
-                    }}
-                />
-            ))}
-        </Marquee>
+            <Marquee
+                pauseOnHover={true}
+                speed={30}
+                direction={direction ? direction : "right"}
+                style={{ overflow: 'visible' }}
+            >
+                {items.map((item, index) => (
+                    <MotionImage
+                        src={`/images/${type}/${item}.jpg`}
+                        width={SIZE}
+                        height={1}
+                        style={{ width: `${SIZE}px`, height: "auto" }}
+                        alt={item}
+                        key={index}
+                        className={styles.item}
+                        whileHover={{
+                            scale: 1.03,
+                            zIndex: 10,
+                            transition: { duration: 0.3 }
+                        }}
+                    />
+                ))}
+            </Marquee>
+
     </div>
 }
