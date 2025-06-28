d9d2846f998dc7fa3ed292b31347e25a7cd10196
Pyramond <pyramond69@gmail.com>
2025-06-28 23:17:18 +0200

    fix: gird does not display

--- src/app/interest/[id]/page.tsx
+++ src/app/interest/[id]/page.tsx
@@ -1,17 +1,12 @@
 import PageTransition from "@/components/PageTransition/PageTransition";
 import Grid from "@/components/interest/grid/Grid";
 
+export default async function GridPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
 
-type Props = {
-    params: {
-        id: string
-    }
-}
-
-export default function GridPage({ params }: Props) {
-    const { id } = params;
-
-    return <PageTransition>
-        {id && <Grid id={id?.toString()} />}
-    </PageTransition>
+    return (
+        <PageTransition>
+            {id && <Grid id={id?.toString()} />}
+        </PageTransition>
+    );
 }
