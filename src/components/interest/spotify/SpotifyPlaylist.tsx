6227331d2ed92bcc21aee9122c839c8d4628e3e2
Pyramond <pyramond69@gmail.com>
2025-09-01 23:14:53 +0200

    feat: add second spotify playlist

--- src/components/interest/spotify/SpotifyPlaylist.tsx
+++ src/components/interest/spotify/SpotifyPlaylist.tsx
@@ -7,11 +7,15 @@ const SpotifyPlaylist: React.FC = async () => {
     const filePath = path.join(process.cwd(), "public", "spotify.json");
     const file = await fs.readFile(filePath, "utf-8");
     const data = JSON.parse(file);
-    const url: string = data["url"];
+    const playlist1: string = data["playlist1"];
+    const playlist2: string = data["playlist2"];
 
-    return <div className={styles.spotifyPlaylist}>
+    return <div>
         <h2 className={styles.title}>Musique</h2>
-        <iframe className={styles.playlist} src={url} width="100%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <div className={styles.spotifyPlaylist}>
+            <iframe className={styles.playlist} src={playlist1} width="90%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" />
+            <iframe className={styles.playlist} src={playlist2} width="90%" height="380" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" />
+        </div>
     </div>
 }
 
